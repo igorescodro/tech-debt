90735550177070f9c50be828f59ff37c63dfff6f
Igor Escodro <escodro@outlook.com>
2026-01-11 12:49:51 -0500

    ♻️ Refactor Maven publishing configuration for annotations and processor modules
    Replaced manual Maven publishing setup with `maven-publish` plugin and
    simplified publishing logic by centralizing to `publishToMavenCentral`
    and `signAllPublications`. Updated group ID to `io.github.igorescodro`.

--- techdebt-processor/build.gradle.kts
+++ techdebt-processor/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
-    id("maven-publish")
+    alias(libs.plugins.maven.publish)
 }
 
-group = "com.escodro.techdebt"
+group = "io.github.igorescodro"
 version = libs.versions.techdebt.get()
 
 repositories {
@@ -29,27 +29,7 @@ kotlin {
     jvmToolchain(17)
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            artifactId = "techdebt-processor"
-            artifact(tasks.jar)
-            pom.withXml {
-                val dependenciesNode = asNode().appendNode("dependencies")
-                configurations.implementation.get().allDependencies.forEach {
-                    val dependencyNode = dependenciesNode.appendNode("dependency")
-                    if (it is ProjectDependency) {
-                        dependencyNode.appendNode("groupId", "com.escodro.techdebt")
-                        dependencyNode.appendNode("artifactId", it.name)
-                        dependencyNode.appendNode("version", libs.versions.techdebt.get())
-                    } else {
-                        dependencyNode.appendNode("groupId", it.group)
-                        dependencyNode.appendNode("artifactId", it.name)
-                        dependencyNode.appendNode("version", it.version)
-                    }
-                    dependencyNode.appendNode("scope", "runtime")
-                }
-            }
-        }
-    }
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
 }
