da2afed1972e1a03857f7b05148823f5952996a9
Igor Escodro <escodro@outlook.com>
2026-01-17 19:50:36 -0500

    ➕ Add Maven Publish plugin alias to `build.gradle.kts`
    Included the alias for the Maven Publish plugin and set as apply false
    to avoid some conflicts while implementing the custom Gradle plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.maven.publish) apply false
     alias(libs.plugins.detekt)
     alias(libs.plugins.ktfmt)
 }
